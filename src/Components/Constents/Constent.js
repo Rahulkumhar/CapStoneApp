3ed887bd8d329d91fcdfc3adfdedb18f605c59cc
Rahulkumhar <rahul.kumar9961@gmail.com>
2023-08-21 13:46:27 +0530

    Sass added and component created

---
+++ src/Components/Constents/Constent.js
@@ -0,0 +1,29 @@
+export const CategoriesData = [
+  {
+    id: 1,
+    title: "Hats",
+    imgUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imgUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imgUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "Women",
+    imgUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "Men",
+    imgUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+  },
+];
+
+export default CategoriesData;
