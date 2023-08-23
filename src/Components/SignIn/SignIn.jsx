e0a2c9ca1286356e1cf45b11b664a8cf8ca0417b
Rahulkumhar <rahul.kumar9961@gmail.com>
2023-08-23 22:53:09 +0530

    firestore added with a user docx

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { signInWithGooglePopup } from '../../Utils/Firebase/firebase.utils';
-
+import React, { useState } from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocFromAuth,
+} from "../../Utils/Firebase/firebase.utils";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocFromAuth(user);
+    console.log("==>", userDocRef);
+  };
+  return (
+    <div>
+      <h1> Yupiieee !! we on Sign in Page.</h1>
+      <button onClick={logGoogleUser}>Sign In with Google</button>
+    </div>
+  );
+};
 
-    const logGoogleUser = async()=>{
-const response = await signInWithGooglePopup();
-console.log('res', response);
-    }
-    return ( 
-        <div>
-        <h1> Yupiieee !! we on Sign in Page.</h1>
-        <button onClick={logGoogleUser}>
-            Sign In with Google
-        </button>
-        </div>
-     );
-}
- 
 export default SignIn;
--- src/Utils/Firebase/firebase.utils.js
+++ src/Utils/Firebase/firebase.utils.js
@@ -1,26 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {
   GoogleAuthProvider,
   getAuth,
   signInWithPopup,
   signInWithRedirect,
 } from "firebase/auth";
+
+import { getDoc, doc, setDoc, getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRipMzFSuWRIrWzRZ2JbmNFuioYiVZxT4",
-  authDomain: "capstone-75ef1.firebaseapp.com",
-  projectId: "capstone-75ef1",
-  storageBucket: "capstone-75ef1.appspot.com",
-  messagingSenderId: "767195526368", //Google project key-767195526368
-  appId: "1:767195526368:web:ab9cff7ec944ac09628c85",
+  apiKey: "AIzaSyCHdgEU4QXDTMn2SM1iG0-PSm6Wd35Ojlk",
+  authDomain: "capclothstore-f8ab1.firebaseapp.com",
+  projectId: "capclothstore-f8ab1",
+  storageBucket: "capclothstore-f8ab1.appspot.com",
+  messagingSenderId: "947241730655",
+  appId: "1:947241730655:web:fd3df0c7308d8964c2d1ea",
+  measurementId: "G-VLJZSLRQLJ",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 const provider = new GoogleAuthProvider();
 
@@ -31,3 +37,28 @@ provider.setCustomParameters({
 export const auth = getAuth();
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
