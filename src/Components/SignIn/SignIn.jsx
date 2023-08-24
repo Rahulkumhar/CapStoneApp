686d113991dc47cc44241303fc7b65986059d3ee
Rahulkumhar <rahul.kumar9961@gmail.com>
2023-08-24 08:48:28 +0530

    Redirect functionality with firestore user data added

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocFromAuth,
+  signInWithGoogleRedirect,
 } from "../../Utils/Firebase/firebase.utils";
 
 const SignIn = () => {
+  useMemo(async () => {
+    const res = await getRedirectResult(auth);
+
+    if (res) {
+      const userDocRef = await createUserDocFromAuth(res.user);
+    }
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocFromAuth(user);
     console.log("==>", userDocRef);
   };
+
   return (
     <div>
       <h1> Yupiieee !! we on Sign in Page.</h1>
       <button onClick={logGoogleUser}>Sign In with Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   );
 };
